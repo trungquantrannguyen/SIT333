957f4922e512fe5cf3a527644df28fc77932b4c3
Trung Quan <tranntquan@gmail.com>
2025-03-18 20:41:48 +1100

    init repo

---
+++ task2_1P/src/main/java/sit707_week2/Main.java
@@ -0,0 +1,13 @@
+package sit707_week2;
+
+/**
+ * Hello world!
+ * @author Ahsan Habib
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        SeleniumOperations.officeworks_registration_page("https://www.officeworks.com.au/app/identity/create-account");
+    }
+}
---
+++ task2_1P/src/main/java/sit707_week2/SeleniumOperations.java
@@ -0,0 +1,80 @@
+package sit707_week2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * This class demonstrates Selenium locator APIs to identify HTML elements.
+ * 
+ * Details in Selenium documentation https://www.selenium.dev/documentation/webdriver/elements/locators/
+ * 
+ * @author Ahsan Habib
+ */
+public class SeleniumOperations {
+
+	public static void sleep(int sec) {
+		try {
+			Thread.sleep(sec*1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void officeworks_registration_page(String url) {
+		// Step 1: Locate chrome driver folder in the local drive.
+		System.setProperty("webdriver.chrome.driver", "/home/mahabib/java_lib/chromedriver-linux64/chromedriver");
+		
+		// Step 2: Use above chrome driver to open up a chromium browser.
+		System.out.println("Fire up chrome browser.");
+		WebDriver driver = new ChromeDriver();
+		
+		System.out.println("Driver info: " + driver);
+		
+		sleep(2);
+	
+		// Load a webpage in chromium browser.
+		driver.get(url);
+		
+		/*
+		 * How to identify a HTML input field -
+		 * Step 1: Inspect the webpage, 
+		 * Step 2: locate the input field, 
+		 * Step 3: Find out how to identify it, by id/name/...
+		 */
+		
+		// Find first input field which is firstname
+		WebElement element = driver.findElement(By.id("firstname"));
+		System.out.println("Found element: " + element);
+		// Send first name
+		element.sendKeys("Ahsan");
+		
+		/*
+		 * Find following input fields and populate with values
+		 */
+		// Write code
+		
+		
+		/*
+		 * Identify button 'Create account' and click to submit using Selenium API.
+		 */
+		// Write code
+		
+		/*
+		 * Take screenshot using selenium API.
+		 */
+		// Write code
+		
+		
+		// Sleep a while
+		sleep(2);
+		
+		// close chrome driver
+		driver.close();	
+	}
+	
+	
+}
